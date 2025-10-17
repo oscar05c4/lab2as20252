bf423d22964c108861b4541bffa26b49ef47e77b
oscar05c4 <oscar.blandon@udea.edu.co>
2025-10-17 14:04:53 -0500

    Repo base

---
+++ src/main/java/com/udea/lab2as20252/Lab2as20252Application.java
@@ -0,0 +1,13 @@
+package com.udea.lab2as20252;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab2as20252Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab2as20252Application.class, args);
+	}
+
+}
